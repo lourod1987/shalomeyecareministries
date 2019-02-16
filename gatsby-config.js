4c406bc6f35b1a554e1ecea65766ad920eddce9a
lourod1987 <louart87@gmail.com>
2019-02-15 23:56:04 -0600

    Changing some of the site metadata to be representative of this project.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Shalom Eyecare Ministries',
     description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    author: 'Luis Rodriguez III'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
