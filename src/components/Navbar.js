cd7920a589bd4a93af7f633ec9f25134e96a7ed9
lourod1987 <louart87@gmail.com>
2019-02-17 20:18:58 -0600

    commenting out unnecessary links and images. Adding in blog link since the blog will no longer be the landing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,18 +50,21 @@ const Navbar = class extends React.Component {
         <Link className="navbar-item" to="/about">
           About
         </Link>
+        <Link className="navbar-item" to="/blog">
+          Blog
+        </Link>
         <Link className="navbar-item" to="/products">
           Products
         </Link>
         <Link className="navbar-item" to="/contact">
           Contact
         </Link>
-        <Link className="navbar-item" to="/contact/examples">
+        {/* <Link className="navbar-item" to="/contact/examples">
           Form Examples
-        </Link>
+        </Link> */}
       </div>
       <div className="navbar-end has-text-centered">
-        <a
+        {/* <a
           className="navbar-item"
           href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
           target="_blank"
@@ -70,7 +73,7 @@ const Navbar = class extends React.Component {
           <span className="icon">
             <img src={github} alt="Github" />
           </span>
-        </a>
+        </a> */}
       </div>
       </div>
     </div>
