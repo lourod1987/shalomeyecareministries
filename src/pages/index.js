477fb30417c77c3189fde713b84aa163327b50bb
lourod1987 <louart87@gmail.com>
2019-03-29 13:29:32 -0500

    changing the main img to a better size for loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import banner from '../../static/img/multitude-of-flowers.jpg'
+import banner from '../../static/img/multitude-of-flowers-1080.png'
 
 export default class IndexPage extends React.Component {
   render() {
